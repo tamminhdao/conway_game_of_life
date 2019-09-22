1390a77de6dbd4c66b1034a51fac136da191cdba
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2019-09-22 17:06:03 -0500

    Validate book selection

--- lib/validation.rb
+++ lib/validation.rb
@@ -2,4 +2,14 @@ class Validation
   def validate(menu, user_input)
     menu.key?(user_input)
   end
+
+  def validate_book_selection(user_input)
+    one_to_ten(user_input)
+  end
+
+  private
+
+  def one_to_ten(user_input)
+    user_input.to_i.positive? && user_input.to_i <= 10
+  end
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -10,23 +10,57 @@ describe Validation do
     }
   end
 
-  it 'returns true when given correct user input' do
-    result = @validator.validate(@menu, '1')
-    expect(result).to be_truthy
+  context '#validate' do
+    it 'returns true when given correct user input' do
+      result = @validator.validate(@menu, '1')
+      expect(result).to be_truthy
+    end
+
+    it 'returns false when given incorrect user input' do
+      result = @validator.validate(@menu, '5')
+      expect(result).to be_falsey
+    end
+
+    it 'returns false when given no user input' do
+      result = @validator.validate(@menu, nil)
+      expect(result).to be_falsey
+    end
+
+    it 'returns false when given a new line as user input' do
+      result = @validator.validate(@menu, "\n")
+      expect(result).to be_falsey
+    end
   end
 
-  it 'returns false when given incorrect user input' do
-    result = @validator.validate(@menu, '5')
-    expect(result).to be_falsey
-  end
+  context '#validate_book_selection' do
+    it 'returns true if the user enters 1' do
+      result = @validator.validate_book_selection('1')
+      expect(result).to be_truthy
+    end
 
-  it 'returns false when given no user input' do
-    result = @validator.validate(@menu, nil)
-    expect(result).to be_falsey
-  end
+    it 'returns true if the user enters 10' do
+      result = @validator.validate_book_selection('10')
+      expect(result).to be_truthy
+    end
 
-  it 'returns false when given a new line as user input' do
-    result = @validator.validate(@menu, "\n")
-    expect(result).to be_falsey
+    it 'returns true if the user enters a number between 1 and 10' do
+      result = @validator.validate_book_selection('7')
+      expect(result).to be_truthy
+    end
+
+    it 'returns false if the user enters 0' do
+      result = @validator.validate_book_selection('0')
+      expect(result).to be_falsey
+    end
+
+    it 'returns false if the user enters a negative number' do
+      result = @validator.validate_book_selection('-10')
+      expect(result).to be_falsey
+    end
+
+    it 'returns false if the user enters not a number' do
+      result = @validator.validate_book_selection('ten')
+      expect(result).to be_falsey
+    end
   end
 end
