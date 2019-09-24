f72a78091099a6ab6c265e1fd1f5e3241807a893
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2019-09-23 19:26:51 -0500

    Execute script

---
+++ bin/execute.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+$LOAD_PATH << File.expand_path('../lib', __dir__)
+require 'timsach'
+
+io = IOadapter.new($stdin, $stdout)
+reading = Reading.new
+search_engine = SearchEngine.new
+presenter = Presenter.new
+validator = Validation.new
+menu = Menu.new(io, validator)
+
+runner = Runner.new(io: io,
+                    reading: reading,
+                    search_engine: search_engine,
+                    presenter: presenter,
+                    validator: validator,
+                    menu: menu)
+
+runner.main_menu_loop
